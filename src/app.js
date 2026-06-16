bee14cbaec82ab5f0f5aeecc2ad138965c4b9ac0
DuboscqDylan <dylan.duboscq@est.fi.uncoma.edu.ar>
2026-06-15 21:42:54 -0300

    Cambios para que funcione correctamente con vercel

--- src/app.js
+++ src/app.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.json({ ok: true });
+});
+
 app.get("/health", (req, res) => {
   res.json({ status: "OK" });
 });
