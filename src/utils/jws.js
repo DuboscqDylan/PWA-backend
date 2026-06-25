0285d6251c6876cfb39fea77b29893027447f9d3
DuboscqDylan <dylan.duboscq@est.fi.uncoma.edu.ar>
2026-06-24 23:47:16 -0300

    Refresh token  y endpoints actualizados

--- src/utils/jws.js
+++ src/utils/jws.js
@@ -1,12 +1,24 @@
 import jwt from "jsonwebtoken";
 
-export const generateToken = (user) => {
+export const generateAccessToken = (user) => {
   return jwt.sign(
     {
       id: user.id,
       email: user.email,
     },
     process.env.JWT_SECRET,
+    {
+      expiresIn: "15m",
+    },
+  );
+};
+
+export const generateRefreshToken = (user) => {
+  return jwt.sign(
+    {
+      id: user.id,
+    },
+    process.env.JWT_REFRESH_SECRET,
     {
       expiresIn: "7d",
     },
